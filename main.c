8fd05aee7f690c9fdbc683b31382c263bf74244a
Muhammad Shameer <86680342+muhammad-shameer@users.noreply.github.com>
2025-11-23 13:47:53 +0500

    Add files via upload

--- main.c
+++ main.c
@@ -21,45 +21,7 @@ void format_date(time_t t, char *buf) {
     strftime(buf, 11, "%Y-%m-%d", tm_info);
 }
 
-// Add new task
-void add_task(GtkWidget *widget, gpointer treeview) {
-    const char *text = gtk_entry_get_text(GTK_ENTRY(entry));
-    if (text[0] == '\0') return;
-
-    GtkTreeIter iter;
-    gtk_list_store_append(store, &iter);
-
-    const gchar *category = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(category_combo));
-    const gchar *priority = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(priority_combo));
-    time_t due = time(NULL) + 86400 * 7; // default due in 7 days
-
-    gtk_list_store_set(store, &iter,
-                       0, text,
-                       1, FALSE,
-                       2, category,
-                       3, priority,
-                       4, due,
-                       -1);
-    gtk_entry_set_text(GTK_ENTRY(entry), "");
-    g_free((gpointer)category);
-    g_free((gpointer)priority);
-
-    gtk_widget_grab_focus(entry);
-}
-
-// Toggle task done
-void toggle_done(GtkCellRendererToggle *cell, gchar *path_str, gpointer treeview) {
-    GtkTreePath *path = gtk_tree_path_new_from_string(path_str);
-    GtkTreeIter iter;
-    gtk_tree_model_get_iter(GTK_TREE_MODEL(store), &iter, path);
-
-    gboolean done;
-    gtk_tree_model_get(GTK_TREE_MODEL(store), &iter, 1, &done, -1);
-    gtk_list_store_set(store, &iter, 1, !done, -1);
-    gtk_tree_path_free(path);
-}
-
-// Update progress bar
+// Update progress bar with a count e.g 1/2 3/5 etc
 void update_progress() {
     GtkTreeIter iter;
     gboolean valid;
@@ -73,11 +35,61 @@ void update_progress() {
         total++;
         valid = gtk_tree_model_iter_next(GTK_TREE_MODEL(store), &iter);
     }
+
     gdouble fraction = total ? ((gdouble)done_count / total) : 0;
     gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(progress_bar), fraction);
+
+    char buf[64];
+    snprintf(buf, sizeof(buf), "%d / %d done", done_count, total);
+    gtk_progress_bar_set_text(GTK_PROGRESS_BAR(progress_bar), buf);
+    gtk_progress_bar_set_show_text(GTK_PROGRESS_BAR(progress_bar), TRUE);
+
+    gtk_widget_queue_draw(progress_bar);
 }
 
-// Row coloring logic
+// Add a new task
+void add_task(GtkWidget *widget, gpointer treeview) {
+    const char *text = gtk_entry_get_text(GTK_ENTRY(entry));
+    if (!text || text[0] == '\0') return;
+
+    GtkTreeIter iter;
+    gtk_list_store_append(store, &iter);
+
+    const gchar *category = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(category_combo));
+    const gchar *priority = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(priority_combo));
+    time_t due = time(NULL) + 86400 * 7; // 7 days is a default due
+
+    gtk_list_store_set(store, &iter,
+                       0, text,
+                       1, FALSE,
+                       2, category,
+                       3, priority,
+                       4, due,
+                       -1);
+
+    gtk_entry_set_text(GTK_ENTRY(entry), "");
+    g_free((gpointer)category);
+    g_free((gpointer)priority);
+    gtk_widget_grab_focus(entry);
+
+    update_progress();
+}
+
+// Mark task done
+void toggle_done(GtkCellRendererToggle *cell, gchar *path_str, gpointer treeview) {
+    GtkTreePath *path = gtk_tree_path_new_from_string(path_str);
+    GtkTreeIter iter;
+    gtk_tree_model_get_iter(GTK_TREE_MODEL(store), &iter, path);
+
+    gboolean done;
+    gtk_tree_model_get(GTK_TREE_MODEL(store), &iter, 1, &done, -1);
+    gtk_list_store_set(store, &iter, 1, !done, -1);
+    gtk_tree_path_free(path);
+
+    update_progress();
+}
+
+// colors based on priority
 void set_row_colors(GtkTreeViewColumn *col, GtkCellRenderer *renderer, GtkTreeModel *model, GtkTreeIter *iter, gpointer data) {
     gchar *priority;
     gboolean done;
@@ -97,7 +109,7 @@ void set_row_colors(GtkTreeViewColumn *col, GtkCellRenderer *renderer, GtkTreeMo
     g_free(priority);
 }
 
-// Save tasks to file
+// Saves tasks to a csv file
 void save_tasks() {
     FILE *fp = fopen("tasks.csv", "w");
     if (!fp) return;
@@ -122,7 +134,7 @@ void save_tasks() {
     fclose(fp);
 }
 
-// loads task from a csv file
+// Load tasks from CSV
 void load_tasks() {
     FILE *fp = fopen("tasks.csv", "r");
     if (!fp) return;
@@ -146,7 +158,7 @@ void load_tasks() {
     fclose(fp);
 }
 
-// deletes a task
+// Delete selected task
 void delete_task(GtkWidget *widget, gpointer treeview) {
     GtkTreeSelection *selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(treeview));
     GtkTreeIter iter;
@@ -168,13 +180,12 @@ int main(int argc, char *argv[]) {
     GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
     gtk_container_add(GTK_CONTAINER(window), vbox);
 
-
-    // task entry
+    // Task entry
     entry = gtk_entry_new();
     gtk_entry_set_placeholder_text(GTK_ENTRY(entry), "Enter new task");
     gtk_box_pack_start(GTK_BOX(vbox), entry, FALSE, FALSE, 5);
 
-    // dropdown for priority
+    // Category dropdown
     category_combo = gtk_combo_box_text_new();
     gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(category_combo), "Personal");
     gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(category_combo), "Work");
@@ -182,7 +193,7 @@ int main(int argc, char *argv[]) {
     gtk_combo_box_set_active(GTK_COMBO_BOX(category_combo), 0);
     gtk_box_pack_start(GTK_BOX(vbox), category_combo, FALSE, FALSE, 5);
 
-    // dropdown for priority selection
+    // Priority dropdown
     priority_combo = gtk_combo_box_text_new();
     gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(priority_combo), "High");
     gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(priority_combo), "Medium");
@@ -190,33 +201,38 @@ int main(int argc, char *argv[]) {
     gtk_combo_box_set_active(GTK_COMBO_BOX(priority_combo), 1);
     gtk_box_pack_start(GTK_BOX(vbox), priority_combo, FALSE, FALSE, 5);
 
-    // add and del buttons
+    // Buttons
     GtkWidget *add_button = gtk_button_new_with_label("Add Task");
     gtk_box_pack_start(GTK_BOX(vbox), add_button, FALSE, FALSE, 5);
 
     GtkWidget *delete_button = gtk_button_new_with_label("Delete Task");
     gtk_box_pack_start(GTK_BOX(vbox), delete_button, FALSE, FALSE, 5);
 
-    // stroes the list
+    // Tree view for tasks
     store = gtk_list_store_new(5, G_TYPE_STRING, G_TYPE_BOOLEAN, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_UINT64);
-
     GtkWidget *treeview = gtk_tree_view_new_with_model(GTK_TREE_MODEL(store));
 
-    // this is to add columns in the section
-    GtkCellRenderer *renderer = gtk_cell_renderer_text_new();
-    GtkTreeViewColumn *col = gtk_tree_view_column_new_with_attributes("Task", renderer, "text", 0, NULL);
+    GtkCellRenderer *renderer;
+    GtkTreeViewColumn *col;
+
+    // Task column
+    renderer = gtk_cell_renderer_text_new();
+    col = gtk_tree_view_column_new_with_attributes("Task", renderer, "text", 0, NULL);
     gtk_tree_view_column_set_cell_data_func(col, renderer, set_row_colors, NULL, NULL);
     gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), col);
 
+    // Done column
     renderer = gtk_cell_renderer_toggle_new();
     g_signal_connect(renderer, "toggled", G_CALLBACK(toggle_done), treeview);
     col = gtk_tree_view_column_new_with_attributes("Done", renderer, "active", 1, NULL);
     gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), col);
 
+    // Category column
     renderer = gtk_cell_renderer_text_new();
     col = gtk_tree_view_column_new_with_attributes("Category", renderer, "text", 2, NULL);
     gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), col);
 
+    // Priority column
     renderer = gtk_cell_renderer_text_new();
     col = gtk_tree_view_column_new_with_attributes("Priority", renderer, "text", 3, NULL);
     gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), col);
@@ -232,6 +248,7 @@ int main(int argc, char *argv[]) {
     g_signal_connect(delete_button, "clicked", G_CALLBACK(delete_task), treeview);
 
     load_tasks();
+    update_progress(); // makes sure the bar shows correct info on start
     gtk_widget_show_all(window);
 
     gtk_main();
